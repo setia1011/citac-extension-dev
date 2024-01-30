86548349bd89513d7e614cd9388a192d14bfef51
setiad1 <dev.setiadi.acc@gmail.com>
2024-01-30 16:45:14 +0700

    add new features

--- src/views/IntelDocSummaryView.vue
+++ src/views/IntelDocSummaryView.vue
@@ -1,8 +1,118 @@
 <template>
   <div ref="main" id="main" class="container is-fluid mt-5 p-32">
-    <page-header></page-header>
-    <div class="columns">
-      <div class="column is-6">
+    <page-header class="is-inline-block"></page-header>
+
+    <!-- Filter -->
+    <div class="is-pulled-right">
+        <button class="button is-small box is-outlined is-info mb-4" data-show="quickview" data-target="quickviewDefault">
+          <span class="icon">
+              <i class="fa-regular fa-filter-list"></i>
+          </span>
+          <!-- <span>Filter</span> -->
+        </button>
+    </div>
+
+    <div id="quickviewDefault" class="quickview">
+         <header class="quickview-header is-light">
+            <p class="title"><i class="fa-regular fa-filter-list"></i></p>
+            <span class="delete" data-dismiss="quickview"></span>
+         </header>
+
+         <div class="quickview-body">
+            <div class="quickview-block">
+               <div class="columns is-multiline m-0">
+
+                  <!-- Date ranges -->
+                  <div class="column pb-0 is-full">
+                     <button ref='calendarTrigger' type='button'>Change</button>
+                  </div>
+
+                  <!-- Filter base -->
+                  <!-- <div class="column pb-0 is-full">
+                     <v-select
+                        class="v-selectx v-komoditi"
+                        placeholder="Komoditi/HS"
+                        v-model="s_base"
+                        label="name"
+                        @update:modelValue="selectedBase"
+                        :reduce="s_base => s_base.code" 
+                        :options="s_base_options">
+                     </v-select>
+                  </div> -->
+
+                     <!-- Komoditi -->
+                     <!-- <div v-if="s_base === 1" class="column pb-0 is-full">
+                        <v-select
+                           class="v-selectx v-filter" 
+                           placeholder="Komoditi" 
+                           v-model="s_komoditi"
+                           label="komoditi"
+                           :reduce="s_komoditi => s_komoditi.komoditi"
+                           @update:modelValue="selectedKomoditi"
+                           @search="getKomoditi"
+                           :options="s_komoditi_options">
+                        </v-select>
+                     </div> -->
+
+                     <!-- Sub-komoditi -->
+                     <!-- <div v-if="s_base === 1" class="column pb-0 is-full">
+                        <v-select 
+                           class="v-selectx v-filter" 
+                           placeholder="Sub-komoditi" 
+                           v-model="s_subkomoditi"
+                           :reduce="s_subkomoditi => s_subkomoditi.subkomoditi"
+                           label="subkomoditi"
+                           @search="subKomoditi"
+                           :options="s_subkomoditi_options">
+                        </v-select>
+                     </div> -->
+
+                     <div v-if="s_base === 2" class="column pb-0 is-full">
+                        <!-- HS -->
+                        <!-- <v-select multiple
+                           class="v-selectx v-filter" 
+                           placeholder="Banyak HS dengan , (12119099, 12129990)" 
+                           v-model="s_hs"
+                           label="kode"
+                           @search="getHs"
+                           :reduce="s_hs => s_hs.kode"
+                           :options="s_hs_options">
+                           <i slot="spinner" class="icon icon-spinner"></i>
+                        </v-select> -->
+                        <v-select multiple taggable push-tags
+                           class="v-selectx v-filter" 
+                           placeholder="Banyak HS dengan , (12119099, 12129990)" 
+                           v-model="s_hs"
+                           label="kode">
+                           <i slot="spinner" class="icon icon-spinner"></i>
+                        </v-select>
+                     </div>
+
+                  <div class="column pb-0 is-full">
+                     <button class="button is-primary" v-on:click="cariCpo($event)">
+                        <span class="icon">
+                           <!-- <i class="fa-brands fa-searchengin"></i> -->
+                           <i class="fa-regular fa-arrow-right-to-arc"></i>
+                        </span>
+                        <span>Go</span>
+                     </button>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <footer class="quickview-footer"></footer>
+      </div>
+
+      <div class="icon-text mb-5">
+         <span class="icon">
+            <i class="fa-sharp fa-regular fa-chart-scatter-bubble"></i>
+         </span>
+         <span class="title is-size-5">Trend Ekspor CPO dan Produk Turunannnya <span class="has-text-info-dark">({{ toIdDate(start_date) }} s.d. {{ toIdDate(end_date) }})</span></span>
+      </div>
+    
+    <!-- <div class="columns">
+      <div class="column">
         <div class="field is-horizontal">
           <div class="field-label is-normal" style="text-align: left; flex-grow: 0 !important; width: 50px;">
             <label class="label">Kantor</label>
@@ -28,23 +138,322 @@
           </div>
         </div>
       </div>
+    </div> -->
+
+    <div class="columns is-multiline is-variable is-1-mobile is-1-tablet is-1-desktop is-1-widescreen is-1-fullhd mt-0">
+      <div class="column is-full-mobile is-full-tablet is-half-desktop is-half-widescreen pb-1 pt-1">
+        <div>
+          <v-chart
+          :key="chart_pie_jenis_dokumen.series"
+          class="chart border" 
+          style="width: 100%; height: 400px;" 
+          :option="chart_pie_jenis_dokumen"
+          autoresize/>
+        </div>
+      </div>
+      <div class="column is-full-mobile is-full-tablet is-half-desktop is-half-widescreen pb-1 pt-1">
+        <v-chart
+          :key="chart_pie_jenis_dokumen.series"
+          class="chart border" 
+          style="width: 100%; height: 400px;" 
+          :option="chart_pie_jenis_dokumen"
+          autoresize/>
+      </div>
+    </div>
+    <div class="columns is-multiline is-variable is-1-mobile is-1-tablet is-1-desktop is-1-widescreen is-1-fullhd mt-0">
+      <div class="column is-full-mobile is-full-tablet is-half-desktop is-half-widescreen pb-1 pt-1">
+
+      </div>
+      <div class="column is-full-mobile is-full-tablet is-half-desktop is-half-widescreen pb-1 pt-1">
+
+      </div>
     </div>
   </div>
 </template>
  
 <script>
+import { ciaxios } from '../utils/xaxios';
+// var _ = require('lodash');
+import _ from 'lodash';
+import * as XLSX from 'xlsx';
+// import debounce from 'lodash.debounce';
+import moment from 'moment';
+import bulmaQuickview from "bulma-extensions/bulma-quickview/dist/js/bulma-quickview";
+import bulmaCalendar from "bulma-extensions/bulma-calendar/dist/js/bulma-calendar";
+import Loading from "vue-loading-overlay";
+import vSelect from "vue-select";
+import { use } from "echarts/core";
+import { CanvasRenderer, SVGRenderer } from "echarts/renderers";
+import { LineChart, BarChart, PieChart, ThemeRiverChart } from "echarts/charts";
+import {
+   VisualMapComponent,
+   GridComponent,
+   TitleComponent,
+   TooltipComponent,
+   LegendComponent,
+   ToolboxComponent,
+   DataZoomComponent,
+   MarkLineComponent
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+
+use([
+   DataZoomComponent,
+   ToolboxComponent,
+   VisualMapComponent,
+   CanvasRenderer,
+   GridComponent,
+   LineChart,
+   BarChart,
+   PieChart,
+   TitleComponent,
+   TooltipComponent,
+   LegendComponent,
+   SVGRenderer,
+   MarkLineComponent,
+   ThemeRiverChart
+]);
 export default {
   name: 'IntelDocSummary',
+  components: {
+    Loading,
+    VChart,
+    vSelect,
+    bulmaQuickview
+  },
   data() {
     return {
       kode_kantor: null,
       start_date: null,
       end_date: null,
       text_search: null,
-      asc: true
+      asc: true,
+      jenis_dokumen: 'all',
+      chart_pie_jenis_dokumen: {
+        title: {
+          text: 'Referer of a Website',
+          subtext: 'Fake Data',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left'
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      },
+      chart_pie_hitrate: {},
+      chart_line_dokumen_per_bulan: {},
+      chart_bar_jenis_dokumen_referensi: {}
     }
   },
   mounted() {
+    var quickviews = bulmaQuickview.attach();
+      const calendar = bulmaCalendar.attach(this.$refs.calendarTrigger, {
+         startDate: this.start_date,
+         endDate: this.end_date,
+         type: 'date',
+         dateFormat: 'DD/MM/YYYY',
+         isRange: true,
+         displayMode: 'default',
+         color: 'primary'
+      })[0]
+
+      calendar.on("hide", event => {
+         this.start_date = event.data.date.start;
+         this.end_date = event.data.date.end;
+         console.log(2)
+      });
+
+      calendar.on("clear", event => {
+         this.start_date = null;
+         this.end_date = null;
+      });
+
+      calendar.on("select", event => {
+         this.start_date = event.data.date.start;
+         this.end_date = event.data.date.end;
+         console.log(1)
+      })
+  },
+  methods: {
+    toXLSX: function(items, fname, ref) {
+         const yx = []
+         if (ref === 'monthly') {
+            items.forEach((e, i) => {
+               e.data.forEach((j, k) => {
+                  yx.push({
+                     'Kluster': e.name,
+                     'Tahun': j[0].split('-')[0],
+                     'Bulan': j[0].split('-')[1],
+                     'Berat (Ton)': j[1]
+                  })
+               })
+            });
+         }
+
+         if (ref === 'weekly') {
+            items.forEach((e, i) => {
+               e.data.forEach((j, k) => {
+                  yx.push({
+                     'Kluster': e.name,
+                     'Tahun': j[0].split('-')[0],
+                     'Minggu': j[0].split('-')[1],
+                     'Berat (Ton)': j[1]
+                  })
+               })
+            });
+         }
+         
+         if (ref === 'daily') {
+            items.forEach((e, i) => {
+               e.data.forEach((j, k) => {
+                  yx.push({
+                     'Kluster': e.name,
+                     'Tahun': j[0].split('-')[0],
+                     'Tanggal': j[0],
+                     'Berat (Ton)': j[1]
+                  })
+               })
+            });
+         }
+         var wb = XLSX.utils.book_new();
+         var ws = XLSX.utils.json_to_sheet(yx);
+         // XLSX.utils.book_append_sheet(wb, ws, 'sheet1');
+         XLSX.utils.book_append_sheet(wb, ws);
+         XLSX.writeFile(wb, fname + ".xlsx");
+      },
+      genDays: function(start, end) {
+         const listDate = [];
+         const startDate = start;
+         const endDate = end;
+         const dateMove = new Date(startDate);
+         let strDate = startDate;
+         while (strDate < endDate) {
+            strDate = dateMove.toISOString().slice(0, 10);
+            listDate.push(strDate);
+            dateMove.setDate(dateMove.getDate() + 1);
+         };
+         return listDate;
+      },
+      toDatabaseDate: function(date) {
+         const year = date?.toLocaleString("id-ID", { year: "numeric" });
+         const month = date?.toLocaleString("id-ID", { month: "2-digit" });
+         const day = date?.toLocaleString("id-ID", { day: "2-digit" });
+         const formattedDate = year + "-" + month + "-" + day;
+         return formattedDate;
+      },
+      toIdDate: function(date) {
+         const year = date?.toLocaleString("id-ID", { year: "numeric" });
+         const month = date?.toLocaleString("id-ID", { month: "2-digit" });
+         const day = date?.toLocaleString("id-ID", { day: "2-digit" });
+         const formattedDate = day + "/" + month + "/" + year;
+         return formattedDate;
+      },
+      hsArr2Str: function() {
+         if (this.s_hs) {
+            var tx = JSON.parse(JSON.stringify(this.s_hs));
+            var qq = [];
+            tx.forEach(e => {
+               qq.push(e);
+            });
+            return qq.join(",");
+         } else {
+            return null;
+         }
+      },
   }
 }
 </script>
+
+
+
+<style>
+@import "vue-select/dist/vue-select.css";
+@import "bulma-extensions/bulma-quickview/dist/css/bulma-quickview.min.css";
+@import "bulma-extensions/bulma-calendar/dist/css/bulma-calendar.min.css";
+@import "bulma-extensions/bulma-divider/dist/css/bulma-divider.min.css";
+
+.chart {
+   width: 100%;
+   min-height: 400px;
+   padding: 15px 13px 13px 13px;
+   border-radius: 4px;
+}
+.loading {
+   border: 1px solid #d9d9d9;
+   border-radius: 4px;
+}
+.v-charte {
+   text-align: center;
+   width: 100%;
+   min-height: 400px;
+   border: 1px solid gainsboro;
+   border-radius: 4px;
+   padding-top: 170px;
+}
+.quickview-header {
+   min-height: 3.3em !important;
+}
+.quickview-footer {
+   border-top: none !important;
+}
+.table td, .table th {
+   font-size: 0.93 !important;
+}
+</style>
+ 
+<style>
+.datetimepicker {
+   z-index: 99999999 !important;
+}
+.datetimepicker-dummy .datetimepicker-clear-button {
+   top: -4px !important;
+}
+.tabs li a {
+   font-weight: bold !important;
+}
+.v-tahun, .v-tahun .vs__dropdown-option, .v-tahun .vs__dropdown-menu {
+   width: 121px !important;
+}
+.v-kantor .vs__dropdown-toggle {
+   border: var(--vs-border-width) var(--vs-border-style) #cc0f3563;
+}
+.v-kantor, .v-kantor .vs__dropdown-option, .v-kantor .vs__dropdown-menu {
+   width: 100% !important;
+}
+.v-komoditi, .v-komoditi .vs__dropdown-option, .v-komoditi .vs__dropdown-menu {
+   width: 150px !important;
+}
+.v-filter, .v-filter .vs__dropdown-option, .v-filter .vs__dropdown-menu {
+   width: 100% !important;
+}
+.v-negara, .v-negara .vs__dropdown-option, .v-negara .vs__dropdown-menu {
+   width: 100% !important;
+}
+.v-exim, .v-exim .vs__dropdown-option, .v-exim .vs__dropdown-menu {
+   width: 150px !important;
+}
+</style>
