f831439fa1792f5ea7b140ff06909ba93e1f4866
setiad1 <dev.setiadi.acc@gmail.com>
2024-02-15 16:37:48 +0700

    Update aja

--- src/utils/xaxios.js
+++ src/utils/xaxios.js
@@ -8,7 +8,12 @@ const imaxios = axios.create({
    baseURL: import.meta.env.DEV ? import.meta.env.VITE_DEV_BASE_URL_API_IMA : import.meta.env.VITE_PROD_BASE_URL_API_IMA
 });
 
+const coaxios = axios.create({
+   baseURL: import.meta.env.DEV ? import.meta.env.VITE_DEV_BASE_URL_API_CITAC : import.meta.env.VITE_PROD_BASE_URL_API_CITAC
+});
+
 export {
    ciaxios,
-   imaxios
+   imaxios,
+   coaxios
 };
--- src/views/IntelDocView.vue
+++ src/views/IntelDocView.vue
@@ -1,14 +1,25 @@
 <template>
-   <div ref="main" id="main" class="container is-fluid mt-5 p-32">
-     <page-header></page-header>
-     ...
-   </div>
+    <div ref="main" id="main" class="container is-fluid mt-5 p-32">
+      <page-header></page-header>
+      ...
+    </div>
 </template>
  
 <script>
+import { coaxios } from '../utils/xaxios';
+import _ from "lodash";
+
 export default {
-name: 'IntelDoc',
-mounted() {
-}
+  name: 'IntelDoc',
+  mounted() {
+    this.fake();
+  },
+  methods: {
+    fake: function() {
+      coaxios.get("/doc?lppi_no=").then((r) => {
+        console.log(r.data);
+      })
+    }
+  }
 }
 </script>
